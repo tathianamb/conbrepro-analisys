df82f0a0feb36fd40119891455cd1aaf8b91379a
tathianamb <tm_barchi@hotmail.com>
2024-08-16 19:56:19 -0300

    add log functions

--- statisticalanalysis.py
+++ statisticalanalysis.py
@@ -1,18 +1,34 @@
 import os
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import adfuller
+import logging
+
+
+# Configuração do logging
+log_file_path = 'STATISTICALANALYSIS/LOG-STATISTICALANALYSIS.txt'
+os.makedirs(os.path.dirname(log_file_path), exist_ok=True)  # Cria o diretório se não existir
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler(log_file_path)]
+)
+
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
 
 def load_data(file_path):
     # Carregar dados do arquivo CSV
+    logging.debug(f'Carregando dados do arquivo: {file_path}')
     data = pd.read_csv(file_path, parse_dates=[0], index_col=0, dayfirst=True)
     return data
 
-
 def plot_time_series(data, title, save_path):
     # Plotar a série temporal
+    logging.debug(f'Plotando a série temporal e salvando a imagem em: {save_path}')
     plt.figure(figsize=(12, 6))
     plt.plot(data, label='Valor')
     plt.title(f'Série Temporal - {title}')
@@ -23,46 +39,47 @@ def plot_time_series(data, title, save_path):
     plt.savefig(save_path, format='png')
     plt.close()
 
-
 def test_stationarity(data):
     # Testar a estacionariedade usando o teste de Dickey-Fuller
+    logging.debug('Realizando o teste de estacionariedade (Dickey-Fuller)')
     result = adfuller(data)
-    print(f'ADF Statistic: {result[0]}')
-    print(f'p-value: {result[1]}')
-    print(f'Critical Values: {result[4]}')
-
+    logging.info(f'ADF Statistic: {result[0]}')
+    logging.info(f'p-value: {result[1]}')
+    logging.info(f'Critical Values: {result[4]}')
 
 def save_acf_img(data, save_path_acf):
     # Plotar ACF
+    logging.debug(f'Plotando ACF e salvando a imagem em: {save_path_acf}')
     plt.figure(figsize=(12, 6))
-    plot_acf(data, lags=40)  # Adjust lags as needed
+    plot_acf(data, lags=40)  # Ajuste o número de lags conforme necessário
     plt.title('ACF')
     plt.savefig(save_path_acf, format='png')
     plt.close()
 
-
 def save_pacf_img(data, save_path_pacf):
     # Plotar PACF
+    logging.debug(f'Plotando PACF e salvando a imagem em: {save_path_pacf}')
     plt.figure(figsize=(12, 6))
-    plot_pacf(data, lags=40)  # Adjust lags as needed
+    plot_pacf(data, lags=40)  # Ajuste o número de lags conforme necessário
     plt.title('PACF')
     plt.savefig(save_path_pacf, format='png')
     plt.close()
-
 
 # Caminho para os arquivos
 load_folder_path = 'DATASET2023-INPUT'
 files = [
     "INMET_CO_DF_A001_BRASILIA_01-01-2023_A_31-12-2023_processed.csv",
-    #"INMET_CO_GO_A002_GOIANIA_01-01-2023_A_31-12-2023_processed.csv",
-    #"INMET_NE_CE_A305_FORTALEZA_01-01-2023_A_31-12-2023_processed.csv",
+    "INMET_CO_GO_A002_GOIANIA_01-01-2023_A_31-12-2023_processed.csv",
+    "INMET_NE_CE_A305_FORTALEZA_01-01-2023_A_31-12-2023_processed.csv",
     "INMET_NE_PI_A312_TERESINA_01-01-2023_A_31-12-2023_processed.csv"
 ]
 
 save_folder_path = 'STATISTICALANALYSIS'
+os.makedirs(save_folder_path, exist_ok=True)  # Cria o diretório se não existir
+
 for file in files:
     file_path = os.path.join(load_folder_path, file)
-    print(f'Analisando o arquivo: {file}')
+    logging.info(f'Analisando o arquivo: {file}')
     data = load_data(file_path)
 
     time_series_img = os.path.join(save_folder_path, f"{file}_time_series.png")
@@ -73,13 +90,10 @@ for file in files:
     plot_time_series(data, os.path.join(save_folder_path, file), time_series_img)
 
     # Testar a estacionariedade
-    print("Teste de Estacionariedade:")
     test_stationarity(data)
 
     # Plotar ACF e salvar a imagem
-    print("Plotando ACF:")
     save_acf_img(data, acf_img)
 
     # Plotar PACF e salvar a imagem
-    print("Plotando PACF:")
     save_pacf_img(data, pacf_img)
