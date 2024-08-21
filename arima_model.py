fba04981c501041a212bfb14be93e2567b6ff45a
tathianamb <tm_barchi@hotmail.com>
2024-08-21 10:50:25 -0300

    removing logs,and adding d as param

--- arima_model.py
+++ arima_model.py
@@ -2,28 +2,7 @@ import numpy as np
 import logging
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
-import warnings
-from statsmodels.tools.sm_exceptions import ConvergenceWarning
 import pandas as pd
-
-
-
-# Configuração do log
-logging.basicConfig(
-    filename='LOG-MODELLING.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    encoding='utf-8'
-)
-
-
-def log_warning(message, category, filename, lineno, file=None, line=None):
-    logging.warning(f'{message} (Category: {category.__name__}, File: {filename}, Line: {lineno})')
-
-warnings.showwarning = log_warning
-
-# Filtrar warnings específicos
-warnings.filterwarnings('always', category=ConvergenceWarning)
 
 
 class ARIMA_Model:
@@ -31,18 +10,21 @@ class ARIMA_Model:
         self.df_train = df_train
         self.df_val = df_val
         self.df_test = df_test
+        self.df_test_predicted = None
+        self.df_val_predicted = None
+        self.df_train_predicted = None
         self.order = None
         logging.info('ARIMA_Model instanciado.')
 
 
-    def grid_search(self):
+    def grid_search(self, d=0):
         gbest_mse = np.inf
         gbest_order = None
 
         for p in range(1, 5):
             for q in range(1, 5):
                 try:
-                    model = ARIMA(self.df_train, order=(p, 1, q))
+                    model = ARIMA(self.df_train, order=(p, d, q))
                     model_fitted = model.fit()
 
                     predictions = model_fitted.predict(start=self.df_val.index[0], end=self.df_val.index[-1])
@@ -55,7 +37,7 @@ class ARIMA_Model:
                         gbest_mse = mse
                         gbest_order = (p, 1, q)
                 except Exception as e:
-                    logging.error(f"Erro ao ajustar o modelo ARIMA(p={p}, d=1, q={q}): {e}", exc_info=True)
+                    logging.error(f"Erro ao ajustar o modelo ARIMA(p={p}, d={d}, q={q}): {e}", exc_info=True)
 
         self.order = gbest_order
         logging.info(f'Melhor modelo encontrado: ARIMA({self.order}) - MSE: {gbest_mse:.4f}')
