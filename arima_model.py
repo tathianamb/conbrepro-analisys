995380d905da3e2e14a0e1743266c86f4e6ff233
tathianamb <tm_barchi@hotmail.com>
2024-08-22 16:05:14 -0300

    Adiciona pré-processamento de dados com escalonamento e métodos para reverter escalonamento no ARIMA_Model
    - Introduz o uso de MinMaxScaler para escalonar os dados de entrada, permitindo a padronização dos intervalos de valores.
    - Implementa o método `scale_data()` para escalonar os conjuntos de dados de treino, validação e teste.
    - Adiciona o método `undo_scale_data()` para reverter a escala dos dados previstos após o ajuste do modelo ARIMA.
    - Inclui o método `run_pipeline()` para executar uma sequência de métodos definidos pelo usuário, facilitando a execução de múltiplos passos do processo.
    - Ajusta a inicialização da classe para aceitar parâmetros de intervalo de escala e refatora o código para suportar as novas funcionalidades.
    
    Essa atualização melhora a flexibilidade do modelo ao lidar com diferentes intervalos de dados e simplifica a execução de múltiplos processos.

--- arima_model.py
+++ arima_model.py
@@ -2,20 +2,38 @@ import numpy as np
 import logging
 from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 
 class ARIMA_Model:
-    def __init__(self, df_train, df_val, df_test):
+    def __init__(self, df_train, df_val, df_test, feature_range = (0, 1)):
         self.df_train = df_train
         self.df_val = df_val
         self.df_test = df_test
-        self.df_test_predicted = None
-        self.df_val_predicted = None
+
+        self.df_train_scaled = None
+        self.df_val_scaled = None
+        self.df_test_scaled = None
+
         self.df_train_predicted = None
+        self.df_val_predicted = None
+        self.df_test_predicted = None
+
+        self.df_train_unscaled = None
+        self.df_val_unscaled = None
+        self.df_test_unscaled = None
+
         self.order = None
+        self.scaler = MinMaxScaler(feature_range=feature_range)
         logging.info('ARIMA_Model instanciado.')
 
+    def scale_data(self):
+        feature_range = (-1, 1)
+        self.df_train_scaled = pd.DataFrame(self.scaler.fit_transform(self.df_train), columns=self.df_train.columns, index=self.df_train.index)
+        self.df_val_scaled = pd.DataFrame(self.scaler.transform(self.df_val), columns=self.df_val.columns, index=self.df_val.index)
+        self.df_test_scaled = pd.DataFrame(self.scaler.transform(self.df_test), columns=self.df_test.columns, index=self.df_test.index)
+        logging.info(f'Dados escalonados para o intervalo: {feature_range}.')
 
     def grid_search(self, d=0):
         gbest_mse = np.inf
@@ -49,3 +67,46 @@ class ARIMA_Model:
         self.df_train_predicted = model_fitted.predict(start=self.df_train.index[0], end=self.df_train.index[-1])
         self.df_val_predicted = model_fitted.predict(start=self.df_val.index[0], end=self.df_val.index[-1])
         self.df_test_predicted = model_fitted.predict(start=self.df_test.index[0], end=self.df_test.index[-1])
+
+    def undo_scale_data(self):
+        if not hasattr(self, 'scaler'):
+            raise AttributeError("Scaler has not been initialized. Please scale the data first.")
+
+        if self.scaler is None:
+            raise ValueError(
+                "Scaler is not set. Please ensure that scaling was performed before attempting to undo it.")
+
+        # Apply inverse transform to revert scaling
+        self.df_train_unscaled = pd.DataFrame(self.scaler.inverse_transform(self.df_train_predicted),
+                                              columns=self.df_train.columns,
+                                              index=self.df_train.index)
+        self.df_val_unscaled = pd.DataFrame(self.scaler.inverse_transform(self.df_val_predicted),
+                                            columns=self.df_val.columns,
+                                            index=self.df_val.index)
+        self.df_test_unscaled = pd.DataFrame(self.scaler.inverse_transform(self.df_test_predicted),
+                                             columns=self.df_test.columns,
+                                             index=self.df_test.index)
+        logging.info(f'Dados revertidos para o intervalo original.')
+
+    def run_pipeline(self, steps):
+        """
+        Execute a sequence of steps specified by the user.
+
+        :param steps: List of steps to execute. Each step should be a callable (method or function) or a string
+                      with the name of the method to call.
+        """
+        available_methods = {method_name: getattr(self, method_name) for method_name in dir(self)
+                             if callable(getattr(self, method_name)) and not method_name.startswith('__')}
+
+        for step in steps:
+            if isinstance(step, str):
+                if step not in available_methods:
+                    raise ValueError(f"Method '{step}' not found in the available methods.")
+                method = available_methods[step]
+            elif callable(step):
+                method = step
+            else:
+                raise ValueError("Steps should be method names (strings) or callable objects.")
+
+            logging.info(f"Method: {method.__name__}")
+            method()
